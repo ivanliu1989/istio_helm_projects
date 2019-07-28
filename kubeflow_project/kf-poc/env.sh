af814020ebfdbdf5c1ad1f3fac6c210d8be1d521
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2019-07-29 00:14:08 +1000

    deploy on existing GKE

---
+++ kubeflow_project/kf-poc/env.sh
@@ -0,0 +1,13 @@
+PLATFORM=null
+KUBEFLOW_REPO=/Users/liut13/projects/istio_helm_projects/kubeflow_project
+KUBEFLOW_VERSION=master
+KUBEFLOW_COMPONENTS='"ambassador","jupyter","notebook-controller","jupyter-web-app","profiles","centraldashboard","tf-job-operator","pytorch-operator","spartakus","argo","pipeline"'
+KUBEFLOW_EXTENDEDINFO=false
+KUBEFLOW_KS_DIR=/Users/liut13/projects/istio_helm_projects/kubeflow_project/kf-poc/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+DOCKER_REGISTRY_KATIB_NAMESPACE=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_PLATFORM=null
+MOUNT_LOCAL=
+DEPLOYMENT_NAME=kf-poc
+
